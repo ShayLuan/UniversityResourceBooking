0e650d8a7571c6aa0109558e48072df0abfbd0c6
Anthony Beaucher <anthony.beaucher@gmail.com>
2025-10-14 19:23:50 -0400

    Fixed registration bug in form validation

--- Project Website/script.js
+++ Project Website/script.js
@@ -1,13 +1,13 @@
 
 /* Catching Mismatching password & its confirmation (Register)*/
-document.getElementById("register-form").addEventListener("submit", function(event) 
-{
-    /* Comparing passwords */
+const registerForm =document.getElementById("register-form");
+if (registerForm){
+     registerForm.addEventListener("submit", function(event) {
     let pw1 = document.getElementById("password").value;
     let pw2 = document.getElementById("confirm-password").value;
     if (pw1 !== pw2) {
         alert("Passwords do not match!"); // An alert pop up
         event.preventDefault(); // Stops the form from submitting
-    }
+        }
+    });
 }
-);
